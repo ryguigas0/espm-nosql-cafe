cadd0ce6bf0ae1482938f34029ba913b488b1925
Guilherme Oliveira Casagrande <32659090+ryguigas0@users.noreply.github.com>
2025-08-29 13:42:06 -0300

    saving cart in redis

--- server.js
+++ server.js
@@ -1,17 +1,35 @@
 import express from "express";
 import path from "node:path";
+import { createClient } from "redis";
+
+const redis = createClient();
+
+redis.on("error", (err) => console.log("Redis Client Error", err));
+
+await redis.connect();
 
 const app = express();
 
 app.use(express.json());
 app.use(express.static(path.join(import.meta.dirname, "assets")));
 
-app.post("/pedido", (req, res) => {
-  const body = req.body;
+app.post("/pedido", async (req, res) => {
+  const items = req.body.items;
 
-  console.log({ body });
+  if (!items || items.length === 0) {
+    res.status(400).json({
+      error: "Missing items",
+    });
+    return;
+  }
 
-  res.status(200).send("ok");
+  const ip = req.ip;
+
+  console.log({ ip, items });
+
+  redis.set(ip, JSON.stringify(items));
+
+  res.status(204).send();
 });
 
 app.listen(3000, async () => {
