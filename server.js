6bbd6cb6e404d6e1c7531656027caad6d28a6b25
Guilherme Oliveira Casagrande <32659090+ryguigas0@users.noreply.github.com>
2025-08-29 13:08:19 -0300

    receiveing frontend create order response

--- server.js
+++ server.js
@@ -3,7 +3,16 @@ const path = require("path");
 
 const app = express();
 
+app.use(express.json())
 app.use(express.static(path.join(__dirname, "assets")));
+
+app.post("/pedido", (req, res) => {
+  const body = req.body;
+
+  console.log({ body });
+
+  res.status(200).send("ok");
+});
 
 app.listen(3000, async () => {
   console.log("Servidor rodando...");
